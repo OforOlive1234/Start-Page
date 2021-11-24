4c3d6623172f3b4233eeafdbfaf0243a320da86a
Olive <69664883+OforOlive1234@users.noreply.github.com>
2021-11-24 21:13:52 +0800

    Weather is now up and working

--- src/index.tsx
+++ src/index.tsx
@@ -11,46 +11,12 @@ const links = [
   {name: "EP", link: "https://www.educationperfect.com/app/#/dashboard/french/"}
 ]
 
-function getWeatherData() {
-  return fetch('https://api.openweathermap.org/data/2.5/weather?q=Perth&appid=e741f2ad8303b4d99654a07f21a7ef9a')
-    .then(response => response.json())
-    .then(data => {
-      return {
-        temp: data.main.temp,
-        description: data.weather[0].description,
-        icon: data.weather[0].icon
-      }
-    }
-  )
-}
-
-function Weather() {
-  const [weather, setWeather] = useState<{temp: any, description: any, icon: any}|null>(null)
-
-  useEffect(() => {
-    getWeatherData().then(data => setWeather(data))
-  }, [])
-
-  if (weather === null) {
-    return <div>Loading...</div>
-  } else {
-    return (
-    <div>
-      <h1>Weather in Perth</h1>
-      <p>{weather.temp}</p>
-      <img src={weather.icon} alt="I'm sure it's a sunny day!" />
-    </div>
-    )
-
-  }
-}
-
 function Links(){
   return (
     <div className="links">
       {links.map(link => (
         <a className="link" href={link.link}>{link.name}</a>
-      ))}
+        ))}
     </div>
   );
 }
@@ -64,7 +30,7 @@ function Time(){
     }, 1000);
     return () => clearInterval(timer);
   }, []);
-      
+  
   return (
     <div className="time">
       <div>{time.getHours().toString().length === 1 ? "0" + time.getHours() : time.getHours()}:{time.getMinutes().toString().length === 1 ? "0" + time.getMinutes() : time.getMinutes()}:{time.getSeconds().toString().length === 1 ? "0" + time.getSeconds() : time.getSeconds()}</div>
@@ -92,6 +58,47 @@ function Time(){
     ][time.getMonth()]} {time.getFullYear().toString()}</div>
     </div>
   );
+}
+
+function getWeatherData() {
+  return fetch('https://api.openweathermap.org/data/2.5/weather?q=Perth&appid=e741f2ad8303b4d99654a07f21a7ef9a')
+    .then(response => response.json())
+    .then(data => {
+      return {
+        temp: data.main.temp,
+        description: data.weather[0].description,
+        icon: data.weather[0].icon
+      }
+    }
+  )
+}
+
+function Weather() {
+  const [weather, setWeather] = useState<{temp: any, description: any, icon: any}|null>(null)
+
+  useEffect(() => {
+    getWeatherData().then(data => setWeather(data))
+  }, [])
+
+  if (weather === null) {
+    return <div>Loading...</div>
+  } else {
+    return (
+    <span className="weather">
+      <img src={
+          (weather.description === "clear sky") ? "http://openweathermap.org/img/wn/01d.png" :
+          (weather.description === "few clouds") ? "http://openweathermap.org/img/wn/02d.png" :
+          (weather.description === "scattered clouds") ? "http://openweathermap.org/img/wn/03d.png" :
+          (weather.description === "broken clouds") ? "http://openweathermap.org/img/wn/04d.png" :
+          (weather.description === "shower rain") ? "http://openweathermap.org/img/wn/09d.png" :
+          (weather.description === "rain") ? "http://openweathermap.org/img/wn/10d.png" :
+          (weather.description === "thunderstorm") ? "http://openweathermap.org/img/wn/11d.png" :
+          (weather.description === "snow") ? "http://openweathermap.org/img/wn/13d.png" :
+          (weather.description === "mist") ? "http://openweathermap.org/img/wn/50d.png": "I'm sure it's a sunny day!"}
+        width="80px" alt={weather.description}/><p className="smallText">{Math.round(weather.temp - 273.15)} &deg;C</p>
+    </span>
+    );
+  }
 }
 
 function App() {
