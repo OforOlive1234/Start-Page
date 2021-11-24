af5ef3a27129cd92213e50d6dd5aba94010db002
OforOlive1234 <69664883+OforOlive1234@users.noreply.github.com>
2021-11-24 17:43:34 +0800

    Progress

---
+++ src/index.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const links = [
+  {name: "Connect", link: "https://connect.det.wa.edu.au/group/students/ui/overview"},
+  {name: "Outlook", link: "https://outlook.office.com/mail/inbox"},
+  {name: "Mathspace", link: "https://mathspace.co"},
+  {name: "GitHub", link: "https://github.com"},
+  {name: "Gmail", link: "https://mail.google.com/mail/u/0/"},
+  {name: "EP", link: "https://www.educationperfect.com/app/#/dashboard/french/"}
+]
+
+function getWeatherData() {
+  return fetch('https://api.openweathermap.org/data/2.5/weather?q=Perth&appid=e741f2ad8303b4d99654a07f21a7ef9a')
+    .then(response => response.json())
+    .then(data => {
+      return {
+        temp: data.main.temp,
+        description: data.weather[0].description,
+        icon: data.weather[0].icon
+      }
+    }
+  )
+}
+
+function Weather() {
+  const [weather, setWeather] = useState<{temp: any, description: any, icon: any}|null>(null)
+
+  useEffect(() => {
+    getWeatherData().then(data => setWeather(data))
+  }, [])
+
+  if (weather === null) {
+    return <div>Loading...</div>
+  } else {
+    return (
+    <div>
+      <h1>Weather in Perth</h1>
+      <p>{weather.temp}</p>
+      <img src={weather.icon} alt="I'm sure it's a sunny day!" />
+    </div>
+    )
+
+  }
+}
+
+function Links(){
+  return (
+    <div className="links">
+      {links.map(link => (
+        <a className="link" href={link.link}>{link.name}</a>
+      ))}
+    </div>
+  );
+}
+
+function Time(){
+  const [time, setTime] = useState(new Date());
+  
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+      
+  return (
+    <div className="time">
+      <div>{time.getHours().toString().length === 1 ? "0" + time.getHours() : time.getHours()}:{time.getMinutes().toString().length === 1 ? "0" + time.getMinutes() : time.getMinutes()}:{time.getSeconds().toString().length === 1 ? "0" + time.getSeconds() : time.getSeconds()}</div>
+    <div className="date">{[
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ][time.getDay()]} {time.getDate().toString()} {[
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ][time.getMonth()]} {time.getFullYear().toString()}</div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+      <Time />
+      <br />
+      <Links />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
